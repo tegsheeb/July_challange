3eb045a1659bababc66fb03f7c46a2b25937224f
tegshee <tegshbayar@gmail.com>
2020-08-21 20:07:44 -0700

    Solve anagram with frequency counter pattern

---
+++ udemy_Algo_DS/anagram.js
@@ -0,0 +1,33 @@
+function validAnagram(str1, str2){
+  // add whatever parameters you deem necessary - good luck!
+  if (str1.length !== str2.length) {
+      return false;
+  }
+
+  const frequency1 = {};
+  const frequency2 = {};
+
+  for (let char of str1) {
+      frequency1[char] = ++frequency1[char] || 1;
+  }
+
+  for (let char of str2) {
+      frequency2[char] = ++frequency2[char] || 1;
+  }
+  console.log(frequency1);
+  console.log(frequency2);
+
+  for (let key in frequency1) {
+      if (frequency2[key] === undefined) {
+          return false;
+      }
+
+      if (frequency1[key] !== frequency2[key]) {
+          return false;
+      }
+  }
+
+  return true;
+}
+
+// console.log(validAnagram('aaz', 'zza'));
