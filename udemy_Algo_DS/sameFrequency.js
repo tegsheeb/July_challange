d32382bf97150d94f5af9bf4fb874c7524deba25
tegshee <tegshbayar@gmail.com>
2020-08-26 10:45:04 -0700

    Solve day 26 and Udemy same Frequency

---
+++ udemy_Algo_DS/sameFrequency.js
@@ -0,0 +1,15 @@
+
+
+function sameFrequency(num1, num2){
+  // good luck. Add any arguments you deem necessary.
+  const lookup = {};
+
+  while(num1 > 9) {
+      let digit = num1 % 10;
+      lookup[digit] ? lookup[digit]++ : lookup
+  }
+
+  return false;
+
+  return true;
+}
