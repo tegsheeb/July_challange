1a632894d2638ee59547a6add5addb878ceb63b8
tegshee <tegshbayar@gmail.com>
2020-09-01 18:45:12 -0700

    Refactor Selection Sort

--- udemy_Algo_DS/selectionSort.js
+++ udemy_Algo_DS/selectionSort.js
@@ -2,11 +2,9 @@ const selectionSort = (arr) => {
   let minIndex, minValue;
   for (let i = 0; i < arr.length; i ++) {
     minIndex = i;
-    minValue = arr[i];
-    for (let j = i; j < arr.length; j++) {
-      if (arr[j] < minValue) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
         minIndex = j;
-        minValue = arr[j];
       }
     }
     if (i !== minIndex) {
