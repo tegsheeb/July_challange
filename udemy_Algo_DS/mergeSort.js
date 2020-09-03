377edf23a31fa943fad5aa2016a8a6e74a126ea4
tegshee <tegshbayar@gmail.com>
2020-09-02 17:22:01 -0700

    Implement mergeSort

---
+++ udemy_Algo_DS/mergeSort.js
@@ -0,0 +1,32 @@
+const mergeSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length/2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+const merge = (left, right) => {
+  let leftIndex = 0;
+  let rigthIndex = 0;
+  let result = [];
+
+  while (leftIndex < left.length && rigthIndex < right.length) {
+    if (left[leftIndex] < right[rigthIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rigthIndex]);
+      rigthIndex++;
+    }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rigthIndex));
+}
+
+// console.log(merge([1,4,9], [2,6,7]));
+// console.log(mergeSort([9, 4, 2, 8, 5]));
