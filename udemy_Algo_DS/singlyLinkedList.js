b51b56d32a08852a2d3df585ef701e1603aff6de
tegshee <tegshbayar@gmail.com>
2020-09-08 20:18:07 -0700

    Implement Linked List push

---
+++ udemy_Algo_DS/singlyLinkedList.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let node = new Node(val);
+    if(!this.head) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length += 1;
+    return this;
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push(1);
+list.push(5);
