b411107da4054b9bbf20ef5cd7dfe7b25fb55e63
tegshee <tegshbayar@gmail.com>
2020-09-08 22:09:31 -0700

    Implement pop Linked List

--- udemy_Algo_DS/singlyLinkedList.js
+++ udemy_Algo_DS/singlyLinkedList.js
@@ -24,8 +24,31 @@ class SinglyLinkedList {
     this.length += 1;
     return this;
   }
+
+  pop () {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let previous = current;
+    while(current.next) {
+      previous = current;
+      current = current.next;
+    }
+    this.tail = previous;
+    this.tail.next = null;
+    this.length--;
+
+    if(this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
 }
 
 let list = new SinglyLinkedList();
 list.push(1);
 list.push(5);
+console.log(list);
+console.log(list.pop());
