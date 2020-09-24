9a7559119d59510044ed71a2d9bfb9de941ebdac
Tegshee Baasan <tegshbayar@gmail.com>
2020-09-23 22:35:06 -0700

    Implement unshift, get, set, insert methods of DDL

--- udemy_Algo_DS/doublyLinkedList.js
+++ udemy_Algo_DS/doublyLinkedList.js
@@ -56,9 +56,73 @@ class DoublyLinkedList {
     this.length--;
     return oldHead;
   }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if(index < 0 || index >= this.length) return null;
+    let counter, target;
+    if(index < this.length / 2) {
+      counter = 0;
+      target = this.head;
+      while(counter < index) {
+        target = target.next;
+        counter++;
+      }
+      return target;
+    } else {
+      counter = this.length - 1;
+      target = this.tail;
+      while(counter > index) {
+        target = target.prev;
+        counter--;
+      }
+    }
+    return target;
+  }
+
+  set(index,val){
+    const foundNode = this.get(index);
+    if(foundNode !== null) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, val){
+    const newNode = new Node(val);
+    if(index < 0 || index > this.length - 1) return false;
+    if(index === 0) return !!this.unshift(val);
+    if(index === this.length) return !!this.push(val);
+
+    const prevNode = this.get(index - 1);
+    const nextNode = prevNode.next;
+    
+    prevNode.next = newNode, newNode.prev = prevNode;
+    newNode.next = nextNode, nextNode.prev = newNode;
+    this.length++;
+    return true;
+  }
+
+
 }
 
 let list = new DoublyLinkedList();
 list.push(25);
 list.push(36);
 list.push(43);
+list.push(57);
+list.push(68);
